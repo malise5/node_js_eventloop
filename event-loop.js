7f3af8ec983241b1b3a53bc6c858fa675b9313b4
Halkano Malise <kudezx@gmail.com>
2023-04-02 11:51:26 +0300

    continuation

--- event-loop.js
+++ event-loop.js
@@ -1,14 +1,16 @@
-const { Console } = require("console");
 const fs = require("fs");
 
-setTimeout(() => {
-  console.log("Timer 1 Finished");
-}, 0);
+setTimeout(() => console.log("Timer 1 Finished"), 0);
 
 setImmediate(() => console.log("Immidiate 1 Finished"));
 
 fs.readFile("test-file.txt", "utf8", (err, data) => {
   console.log("I/O finished");
+  console.log("================================================");
+
+  setTimeout(() => console.log("Timer 2 Finished"), 0);
+  setTimeout(() => console.log("Timer 3 Finished"), 3000);
+  setImmediate(() => console.log("Immidiate 2 Finished"));
 });
 
 console.log("Hallo from the top level");
