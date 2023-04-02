ce26b057dc6b3a72fd387295dbeaa4ec5d18b36b
Halkano Malise <kudezx@gmail.com>
2023-04-02 12:54:52 +0300

    news stream file created

---
+++ streams.js
@@ -0,0 +1,3 @@
+//streams are used to process (read and write ) data piece  by piece (chunks)
+//without completing the whole read or write operation,
+// and therefore without keeping all the data in memoryUsage
