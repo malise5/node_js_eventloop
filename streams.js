11cf069215e759c0f17a319af5a5b3035435650c
Halkano Malise <kudezx@gmail.com>
2023-04-02 13:51:11 +0300

    explore three solution on how to use streams

--- streams.js
+++ streams.js
@@ -1,3 +1,33 @@
 //streams are used to process (read and write ) data piece  by piece (chunks)
 //without completing the whole read or write operation,
-// and therefore without keeping all the data in memoryUsage
+// and therefore without keeping all the data in memory
+
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  // ..solution1
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+
+  //   //   ..solution 2
+  //   const readable = fs.createReadStream("test-file.txt");
+  //   readable.on("data", (chunk) => {
+  //     res.write(chunk);
+  //   });
+  //   readable.on("end", () => {
+  //     res.end();
+  //   });
+
+  //Solution 3 pipe
+  const readable = fs.createReadStream("text-file.txt");
+  readable.pipe(res);
+});
+
+server.listen(8000, () => {
+  console.log("Waiting for a request on port 8000");
+});
